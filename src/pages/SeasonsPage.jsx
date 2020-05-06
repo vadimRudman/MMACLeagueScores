8dc0297b803ef6db8969bf8de7693f9ce0552916
Vadim Rudman <vadim.rudman@appdirect.com>
2020-05-06 08:02:40 -0400

    Add file structure and SeasonPage

---
+++ src/pages/SeasonsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonsPage = () => (
+	<div>
+        <h1>MMAC Basketball</h1>
+        <h2>Choose Season</h2>
+        <button>Add Season</button>    
+    </div>
+);
+
+export default SeasonsPage;
